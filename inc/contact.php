3d75917c9ce3f24394871daffb73703d1242792e
Richard Levitte <richard@levitte.org>
2013-11-14 05:38:55 +0100

    Reformat to my liking.  (what was up with that extra addition of k-nav-current???)

--- inc/contact.php
+++ inc/contact.php
@@ -1,41 +1,42 @@
 <koken:head>
-    <title>Contact - domain.tld</title>
+    <title>Kontakt - LePhoto.se</title>
 </koken:head>
 
 <koken:load>
     <div id="text">
         <article>
             <header>
-                <time>July 2, 2013</time>
-                <h2>Contact me</h2>
+                <h2>Kontakt</h2>
             </header>
             <p id="headline"></p>
 
-            <form action="../helpers/webmailer.php" id="form" method="post" class="contactform">
+            <form action="/storage/themes/LePhoto/helpers/webmailer.php" id="form" method="post" class="contactform">
                 <p class="input">
-                    <label for="contactName">Your name:</label>
+                    <label for="contactName">Ditt namn:</label>
                     <input type="text" name="name" id="name" style="width: 99%; font-size: 0.95em;"/>
                 </p>
 
                 <p class="input">
-                    <label for="email">Your e-mail:</label>
+                    <label for="email">Din epost:</label>
                     <input type="email" name="email" id="email" style="width: 99%; font-size: 0.95em;"/>
                 </p>
 
-                <img id="captchaImage" src="../helpers/captcha.php" alt="captcha" style="margin-top: -1em;"/>
+                <img id="captchaImage" src="/storage/themes/LePhoto/helpers/captcha.php" alt="captcha" style="position:relative; float:left; margin-right:10px;"/>
 
                 <p class="input">
-                    <label for="captcha">Verification code:</label>
-                    <input type="text" name="captcha" id="captcha" style="width: 99%; font-size: 0.95em;"/>
+                    <label for="captcha">Bekräfta koden till vänster:</label><br />
+                    <input type="text" name="captcha" id="captcha" style="width: 7ex; font-size: 0.95em;"/>
                 </p>
 
+		<hr style="width:100%;clear:both;visibility:hidden;">
+
                 <p class="textarea">
-                    <label for="message">Your message:</label>
+                    <label for="message">Meddelande:</label>
                     <textarea name="message" id="message" rows="5" style="width: 99%; font-size: 0.95em;"></textarea>
                 </p>
 
                 <p class="button">
-                    <button type="submit" id="submitButton">Send message</button>
+                    <button type="submit" id="submitButton">Skicka</button>
                 </p>
 
                 <input type="hidden" name="operation" id="operation" value="submit"/>
@@ -44,16 +45,10 @@
     </div>
     <script>
         jQuery('#captchaImage').bind('load', function () {
-            $('nav>ul').each(
-                // sets the correct sub page as selected in header and footer
-                function (index, value) {
-                    $(value).children().last().children().first().addClass('k-nav-current');
-                }
-            );
             // gets the validation of a former request of webmailer.php
             $.ajax({
                 type: "POST",
-                url: "../helpers/webmailer.php",
+                url: "/storage/themes/LePhoto/helpers/webmailer.php",
                 data: {"operation": "notification"},
                 success: function (data) {
                     switch (data.status) {
